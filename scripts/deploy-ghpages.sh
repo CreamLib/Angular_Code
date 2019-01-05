7451f77c180149ace44af30a582d7dd89bbee787
Emmanuel Demey <demey.emmanuel@gmail.com>
2019-01-05 14:24:16 +0100

    fix: wrong branch

--- scripts/deploy-ghpages.sh
+++ scripts/deploy-ghpages.sh
@@ -50,7 +50,7 @@ git add -A
 # now commit, ignoring branch gh-pages doesn't seem to work, so trying skip
 git commit --allow-empty -m "Deploy to GitHub pages [ci skip]"
 # and push, but send any output to /dev/null to hide anything sensitive
-git push --force --quiet origin gh-pages
+git push --force --quiet origin master
 # go back to where we started and remove the gh-pages git repo we made and used
 # for deployment
 cd ..
