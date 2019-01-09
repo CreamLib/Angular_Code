8d478d2ad28a961621b6dd4632ef0b14879369b6
Johan BEKASINSKI <johan.bekasinski@gmail.com>
2019-01-09 09:08:24 +0100

    feat: moove input float label component to lib

--- projects/creamnglib/src/lib/creamnglib.module.ts
+++ projects/creamnglib/src/lib/creamnglib.module.ts
@@ -1,14 +1,15 @@
-import { NgModule } from "@angular/core";
-import { ButtonComponent } from "./button/button.component";
-import { ButtonFileComponent } from "./button-file/button-file.component";
-import { ButtonIconComponent } from "./button-icon/button-icon.component";
-import { ButtonIconOnlyComponent } from "./button-icon-only/button-icon-only.component";
-import { ButtonSubmitComponent } from "./button-submit/button-submit.component";
-import { BrowserModule } from "@angular/platform-browser";
-import { ToastComponent } from "./toast/toast.component";
-import { InputComponent } from "./input/input.component";
-import { CarouselComponent } from "./carousel/carousel.component";
-import { CarouselItemComponent } from "./carousel/carousel-item/carousel-item.component";
+import { NgModule } from '@angular/core';
+import { ButtonComponent } from './button/button.component';
+import { ButtonFileComponent } from './button-file/button-file.component';
+import { ButtonIconComponent } from './button-icon/button-icon.component';
+import { ButtonIconOnlyComponent } from './button-icon-only/button-icon-only.component';
+import { ButtonSubmitComponent } from './button-submit/button-submit.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { ToastComponent } from './toast/toast.component';
+import { InputComponent } from './input/input.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { CarouselItemComponent } from './carousel/carousel-item/carousel-item.component';
+import { InputFloatingComponent } from './input-floating/input-floating.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { CarouselItemComponent } from "./carousel/carousel-item/carousel-item.co
     ToastComponent,
     InputComponent,
     CarouselComponent,
-    CarouselItemComponent
+    CarouselItemComponent,
+    InputFloatingComponent
   ],
   imports: [BrowserModule],
   exports: [
@@ -32,7 +34,8 @@ import { CarouselItemComponent } from "./carousel/carousel-item/carousel-item.co
     ToastComponent,
     InputComponent,
     CarouselComponent,
-    CarouselItemComponent
+    CarouselItemComponent,
+    InputFloatingComponent
   ]
 })
 export class CreamnglibModule {}
