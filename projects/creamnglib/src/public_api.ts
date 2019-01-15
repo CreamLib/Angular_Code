50540463dc635aeb2c272966c07f71c1d2673b5d
Johan BEKASINSKI <johan.bekasinski@gmail.com>
2019-01-15 16:26:09 +0100

    feat: moove modal alert component into lib

--- projects/creamnglib/src/public_api.ts
+++ projects/creamnglib/src/public_api.ts
@@ -2,12 +2,42 @@
  * Public API Surface of creamnglib
  */
 
-export * from "./lib/button/button.component";
-export * from "./lib/button-file/button-file.component";
-export * from "./lib/button-icon/button-icon.component";
-export * from "./lib/button-icon-only/button-icon-only.component";
-export * from "./lib/button-submit/button-submit.component";
-export * from "./lib/toast/toast.component";
-export * from "./lib/modal/modal.component";
+export * from './lib/button/button.component';
+export * from './lib/button-file/button-file.component';
+export * from './lib/button-icon/button-icon.component';
+export * from './lib/button-icon-only/button-icon-only.component';
+export * from './lib/button-submit/button-submit.component';
+export * from './lib/toast/toast.component';
 
-export * from "./lib/creamnglib.module";
+export * from './lib/accordion/accordion.component';
+export * from './lib/accordion/accordion-item/accordion-item.component';
+export * from './lib/button/button.component';
+export * from './lib/calendar/calendar.component';
+export * from './lib/carousel/carousel.component';
+export * from './lib/carousel/carousel-item/carousel-item.component';
+export * from './lib/checkbox/checkbox.component';
+export * from './lib/checkbox-button/checkbox-button.component';
+export * from './lib/checkbox-group/checkbox-group.component';
+export * from './lib/checkbox-group/checkbox-group-item/checkbox-group-item.component';
+export * from './lib/checkbox-link/checkbox-link.component';
+export * from './lib/dropdown/dropdown.component';
+export * from './lib/dropdown/dropdown-item/dropdown-item.component';
+export * from './lib/fieldset/fieldset.component';
+export * from './lib/input/input.component';
+export * from './lib/input-error-msg/input-error-msg.component';
+export * from './lib/input-floating/input-floating.component';
+export * from './lib/input-mail/input-mail.component';
+export * from './lib/input-material/input-material.component';
+export * from './lib/input-password/input-password.component';
+export * from './lib/input-search/input-search.component';
+export * from './lib/input-tel/input-tel.component';
+export * from './lib/input-url/input-url.component';
+export * from './lib/list/list.component';
+export * from './lib/list-description/list-description.component';
+export * from './lib/list-order/list-order.component';
+export * from './lib/list-thumbnails/list-thumbnails.component';
+
+export * from './lib/modal/modal.component';
+export * from './lib/modal-alert/modal-alert.component';
+
+export * from './lib/creamnglib.module';
