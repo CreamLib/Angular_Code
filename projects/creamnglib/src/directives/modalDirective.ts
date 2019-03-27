54054a6e26b38d74ae43beb76df50b66d3b34ee9
Johan BEKASINSKI <johan.bekasinski@gmail.com>
2019-03-27 17:03:34 +0100

    Feat: directive focus out modal

--- projects/creamnglib/src/directives/modalDirective.ts
+++ projects/creamnglib/src/directives/modalDirective.ts
@@ -1,54 +1,39 @@
-import { Directive, ElementRef, Renderer } from '@angular/core';
+import { Directive, ElementRef, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[c3mModalOut]'
 })
 export class ModalDirective {
-  constructor(private el: ElementRef, private renderer: Renderer) {
+  constructor(private el: ElementRef, private renderer: Renderer2) {
     const map = {};
-
-    console.log('Directive');
-    console.log(this.el.nativeElement);
-    const nbEltPopin = this.el.nativeElement.parentElement;
-    console.log('Nb enfants: ' + nbEltPopin);
+    let oldElementFocused;
+    const containerModal = this.el.nativeElement;
     this.el.nativeElement.onkeydown = this.el.nativeElement.onkeyup = function(e) {
+      const elementFocused = <HTMLElement>e.target;
+      const typeElementFocused = elementFocused;
+
+      const nbChild = containerModal.childElementCount;
+      const lastChildElt = containerModal.childNodes[nbChild - 1];
+      const firstChildElt = containerModal.childNodes[0];
+
       e = e || event; // to deal with IE
+
       map[e.keyCode] = e.type === 'keydown';
+
       if (map[16] && map[9]) {
-        // CTRL+SHIFT+A
-        console.log('On essaie de remonter');
-        this.el.nativeElement.focus();
+        // Test if we try to focus ahead of modal title
+        if (oldElementFocused.nodeName === 'H1') {
+          lastChildElt.focus();
+          e.preventDefault();
+        }
+      } else if (map[9] && !map[16]) {
+        // Test if we try to focus after close button
+        if (oldElementFocused.nodeName === 'BUTTON' && oldElementFocused.className === 'dialog-close') {
+          firstChildElt.focus();
+          e.preventDefault();
+        }
       }
+      oldElementFocused = typeElementFocused;
     };
-    /* this.el.nativeElement.addEventListener("keydown", (event) => {
-      if (event.defaultPrevented) {
-        return; // Ne devrait rien faire si l'événement de la touche était déjà consommé.
-      }
-      console.log(event.key);
-      }
-      switch (event.key) {
-        case "ArrowDown":
-          console.log('ArrowDown');
-          break;
-        case "ArrowUp":
-          // Faire quelque chose pour la touche "up arrow" pressée.
-          break;
-        case "ArrowLeft":
-          // Faire quelque chose pour la touche "left arrow" pressée.
-          break;
-        case "ArrowRight":
-          // Faire quelque chose pour la touche "right arrow" pressée.
-          break;
-        case "Enter":
-          // Faire quelque chose pour les touches "enter" ou "return" pressées.
-          break;
-        case "Escape":
-          // Faire quelque chose pour la touche "esc" pressée.
-          break;
-        default:
-          return; // Quitter lorsque cela ne gère pas l'événement touche.
-      }
-      event.preventDefault();
-    }); */
   }
 }
